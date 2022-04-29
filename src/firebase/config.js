0796d7a3d5cee3160bd46f843af60bc5789650ea
pierrebonnet78 <60656840+pierrebonnet78@users.noreply.github.com>
2022-04-29 15:48:34 +0200

    firebase backend login/register

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxBSbFhtug3PKV7Q-frWSHkK-KMewpqWs",
+  authDomain: "my-doctor-app-79280.firebaseapp.com",
+  projectId: "my-doctor-app-79280",
+  storageBucket: "my-doctor-app-79280.appspot.com",
+  messagingSenderId: "542961511083",
+  appId: "1:542961511083:web:a32a341d138f6ec8ae65ee",
+  measurementId: "G-YKQH5F5G5L",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const authentification = getAuth(app);
+export const db = getFirestore(app);
